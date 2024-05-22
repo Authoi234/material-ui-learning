19ab0fe543baa81d8664c27bcec45b2acbd1e063
Authoi234 <jawadkhan.authoi@gmail.com>
2024-05-23 00:38:40 +0700

    this is a practise of material ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,127 @@
-import logo from './logo.svg';
+import { Avatar, Box, Button, CssBaseline, Divider, Fab, IconButton, Paper, Rating, Switch, TextField, ThemeProvider, Typography, createTheme } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
 import './App.css';
+import { useEffect, useState } from 'react';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import AddIcon from '@mui/icons-material/Add';
+import HeroCard from './Components/HeroCard';
 
 function App() {
+  const [submitting, setSubmitting] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [isActive, setIsActive] = useState(0);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
+  const theme1 = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#ff0000'
+      }
+    }
+  })
+  const theme2 = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#ffff00'
+      }
+    }
+  })
+
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setSubmitting(false)
+    }, 1000);
+
+    return () => {
+      clearTimeout(id);
+    }
+  }, [submitting])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkMode ? theme2 : theme1}>
+      <Switch onClick={() => setDarkMode(!darkMode)} />
+      <div className="App" style={{ margin: '8% 15%' }}>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '1 rem' }}>
+          <Avatar alt="Remy Sharp" src="https://a.espncdn.com/combiner/i?img=/i/headshots/soccer/players/full/45843.png&w=350&h=254" sx={{ width: '80px', height: '80px' }} />
+          <Typography variant='h4' component='h5'>
+            Lionel Messi
+          </Typography>
+        </div>
+        <Button variant='contained' fullWidth color='secondary' sx={{
+          mb: 2,
+          mt: 2
+        }}>Learn More</Button>
+        <LoadingButton loading={submitting} variant="outlined" onClick={() => setSubmitting(true)}>
+          Fetch data
+        </LoadingButton>
+        <Divider />
+        <Typography variant='h5'>
+          Contact Us
+        </Typography>
+        <InstagramIcon color='primary'></InstagramIcon>
+        <IconButton color="primary" aria-label="add to shopping cart">
+          <AddShoppingCartIcon />
+        </IconButton>
+        <Paper sx={{
+          height: 600,
+          width: 600,
+          display: 'grid',
+          placeItems: 'center'
+        }}>
+          <CssBaseline />
+          <HeroCard></HeroCard>
+          <Paper sx={{width: '100px', height: '100px', mt: '20px'}}>
+
+          </Paper>
+        </Paper>
+        <Divider sx={{ mt: 3, mb: 3 }} />
+        <Fab color="secondary" aria-label="add">
+          <AddIcon />
+        </Fab>
+        <Rating
+          sx={{ color: 'cyan' }}
+          name="simple-controlled"
+          value={rating}
+          onChange={(event, newValue) => {
+            setRating(newValue);
+          }}
+        />
+        <div>
+          <TextField label='Your hobby' />
+          <div>
+            <Button variant='contained' sx={[{
+              border: '1px solid red',
+              bgcolor: 'steelblue',
+              m: '10px 3px',
+              p: 2,
+              width: [
+                100,
+                200,
+                300,
+                400,
+                500
+              ],
+              '$.MuiButton-root': {
+                color: 'red'
+              }
+            },
+            isActive && {
+              background: 'yellow'
+            }
+            ]}>Submit</Button>
+            <Switch onClick={() => { isActive ? setIsActive(false) : setIsActive(true) }} {...label} />
+          </div>
+          <Button variant='contained'>Cancel</Button>
+
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Components/HeroCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Box, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+
+const HeroCard = () => {
+    return (
+        <Card sx={{ maxWidth: 345, p: 2 }}>
+            <CardMedia
+                sx={{ height: 340 }}
+                image="https://www.ecologyasia.com/images-ab/blue-crested-lizard_7U1A2702_CCa.jpg"
+                title="Lizard"
+            />
+            <CardContent sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+            }}>
+                <Box>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptile
+                    </Typography>
+                </Box>
+            </CardContent>
+            <IconButton sx={{border: '1px solid black'}}>
+                <AddIcon></AddIcon>
+            </IconButton>
+        </Card>
+    );
+};
+
+export default HeroCard;
